d8a99022075c38b55c6b6697c289bf217ea2091f
Lavish <lalitchoudhary112000@gmail.com>
2025-12-23 23:02:06 +0530

    Refactor: remove unused req parameter in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware() {
   const res = NextResponse.next();
 
   res.headers.set(
